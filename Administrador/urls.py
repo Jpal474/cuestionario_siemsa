87d69ec8fdf72ca2a39ad2973d2d8ecc254a2a0f
DanYen7 <danielaaf2000@outlook.es>
2023-04-19 10:32:54 -0600

    Cambios a Administrador

--- Administrador/urls.py
+++ Administrador/urls.py
@@ -3,9 +3,18 @@ from django.urls import path
 
 urlpatterns = [
     path('inicio/', Administrador.views.inicio, name="inicio_administrador"),
-    path('cuestionario/', Administrador.views.cuestionario, name="cuetionario_administrador"),
+    path('cuestionario/', Administrador.views.cuestionario, name="cuestionario_administrador"),
     path('investigacion/', Administrador.views.investigacion, name="investigacion_administrador"),
-    path('editar_pregunta/', Administrador.views.editar_investigacion, name="editar_pregunta")
-
-    
+    path('editar_pregunta/', Administrador.views.editar_investigacion, name="editar_pregunta"),
+    path("usuarios/", Administrador.views.ver_usuarios, name="ver_usuarios"),
+    path("respuestas/", Administrador.views.respuestas, name="respuestas"),
+    path("nomenclatura/", Administrador.views.nomenclatura, name="nomenclatura"),
+    path('preguntas_investigacion/', Administrador.views.preguntas_investigacion, name="preguntas_investigacion"),   
+    path('preguntas_desarrollo/', Administrador.views.preguntas_desarrollo, name="preguntas_desarrollo"),   
+    path('preguntas_integracion/', Administrador.views.preguntas_integracion, name="preguntas_integracion"),   
+    path('preguntas_propiedad/', Administrador.views.preguntas_propiedad, name="preguntas_propiedad"),   
+    path('preguntas_normatividad/', Administrador.views.preguntas_normatividad, name="preguntas_normatividad"),   
+    path('preguntas_manufactura/', Administrador.views.preguntas_manufactura, name="preguntas_manufactura"),   
+    path('preguntas_usuarios/', Administrador.views.preguntas_usuarios, name="preguntas_usuarios"),   
+    path('preguntas_aspectos/', Administrador.views.preguntas_aspectos, name="preguntas_aspectos"),        
 ]
--- Administrador/views.py
+++ Administrador/views.py
@@ -13,5 +13,35 @@ def investigacion(request):
 def editar_investigacion(request):
     return render (request, 'editar_investigacion.html')
 
+def ver_usuarios(request):
+    return render (request, 'usuarios.html')
 
+def respuestas(request):
+    return render (request, 'respuestas.html')
 
+def nomenclatura(request):
+    return render (request, 'nomenclatura.html')
+
+def preguntas_investigacion(request):
+    return render (request, 'preguntas_investigacion.html')
+
+def preguntas_desarrollo(request):
+    return render (request, 'preguntas_desarrollo.html')
+
+def preguntas_integracion(request):
+    return render (request, 'preguntas_integracion.html')
+
+def preguntas_propiedad(request):
+    return render (request, 'preguntas_propiedad.html')
+
+def preguntas_normatividad(request):
+    return render (request, 'preguntas_normatividad.html')
+
+def preguntas_manufactura(request):
+    return render (request, 'preguntas_manufactura.html')
+
+def preguntas_usuarios(request):
+    return render (request, 'preguntas_usuarios.html')
+
+def preguntas_aspectos(request):
+    return render (request, 'preguntas_aspectos.html')
